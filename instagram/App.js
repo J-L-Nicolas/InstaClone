1de3a202f3e5ee23554cf7a2c6714557bc54214d
LEMONY <jeremylemony_dwwm20@imfpa.mq>
2021-07-05 16:22:32 -0400

    add icon test

--- instagram/App.js
+++ instagram/App.js
@@ -1,10 +1,17 @@
 import React from 'react'
 import { View, Text } from 'react-native'
 
+/* inport install components */
+import Icon from 'react-native-vector-icons/FontAwesome';
+
+
 const App = () => {
+  
+  const myIcon = <Icon name="rocket" size={30} color="#900" />;
+
   return (
     <View>
-      <Text>APP Super</Text>
+      <Text>APP Super{myIcon}</Text>
     </View>
   )
 }
