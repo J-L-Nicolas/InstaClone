caa48f16b467383e82f4725ad0e2b84ae590392f
LEMONY <jeremylemony_dwwm20@imfpa.mq>
2021-07-06 11:55:06 -0400

    update components login

--- instagram/components/Login/index.js
+++ instagram/components/Login/index.js
@@ -1,10 +1,46 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React, {useState} from 'react'
+import { View, Text, Image,  TextInput , Button , Pressable } from 'react-native'
+import styles from './styles'
 
 const Login = () => {
+
+    /* state */
+    const [login, setlogin] = useState("")
+    const [password, setpassword] = useState("")
+
     return (
-        <View>
-            <Text></Text>
+        <View style={styles.bodyLogin}>
+         
+            <Image
+                source={require('../../Assets/Images/bg_login.jpg')}
+            />
+
+            <View style={styles.bodyLogo}>
+
+                <Image style={styles.Logo}
+                    source={require('../../Assets/Images/logo_Insta.png')}
+                />
+
+            </View>
+
+            <View style={styles.bodyForm}>
+                <TextInput
+                    placeholder="Login"
+                    style={styles.input}
+                    onChangeText={setlogin}
+                    value={login}
+                />
+                 <TextInput
+                 placeholder="Password"
+                    style={styles.input}
+                    onChangeText={setpassword}
+                    value={password}
+                />
+                <Pressable style={styles.btn}>
+                    <Text>Connexion</Text>
+                </Pressable>
+            </View>
+         
         </View>
     )
 }
