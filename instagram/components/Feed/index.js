8c4abb4c99b5e6efad9e8af820fa0a7bb894ca07
LEMONY <jeremylemony_dwwm20@imfpa.mq>
2021-07-06 15:22:28 -0400

    add components feed

---
+++ instagram/components/Feed/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+import styles from './styles'
+
+const Feed = () => {
+    return (
+        <View>
+            <Text>Fedd</Text>
+        </View>
+    )
+}
+
+export default Feed
